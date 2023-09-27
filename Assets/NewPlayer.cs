c8c9d93dda7d3011a0b5836f257cfb8b71051ca5
AndersDahlenborg <anders.dahlenborg@symphony.com>
2023-09-27 09:10:59 +0200

    new animator var

--- Assets/NewPlayer.cs
+++ Assets/NewPlayer.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class NewPlayer : MonoBehaviour
 {
     private float moveSpeed = 2;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Jump();
+
+        animator.SetBool("isRunning", false);
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
