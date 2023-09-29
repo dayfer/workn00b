9a5dcfe81f0607245c81ef383f2aeec1221a9c6b
Anders Dahlenborg <anders@daylon.se>
2023-09-29 13:27:09 +0200

    just new stuff

--- Assets/WarriorPlayer.cs
+++ Assets/WarriorPlayer.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewPlayer : MonoBehaviour
+public class WarriorPlayer : MonoBehaviour
 {
     private float moveSpeed = 2;
     Animator animator;
-    
+    bool noseToTheEast = true;
+
 
 
     // Start is called before the first frame update
@@ -22,24 +23,37 @@ public class NewPlayer : MonoBehaviour
         animator.SetBool("isRunning", false);
 
         float fInputH = Input.GetAxis("Horizontal");
+
+
         Vector3 movement = new Vector3(fInputH, 0f, 0f);
-        if(fInputH != 0)
+        
+        if (fInputH != 0.0)
         {
             animator.SetBool("isRunning", true);
             transform.position += movement * Time.deltaTime * moveSpeed;
+
+            if (fInputH < 0.0 && noseToTheEast)
+            {
+                noseToTheEast = true;
+                Vector3 currentScale = transform.localScale;
+                currentScale.x = -currentScale.x;
+                transform.localScale = currentScale;
+
+            }
+            noseToTheEast = fInputH > 0.0;
+
+
         }
 
 
 
         float fInputV = Input.GetAxis("Vertical");
         Vector3 movement2 = new Vector3(0f, fInputV, 0f);
-        if (fInputV != 0)
+        if (fInputV != 0.0)
         {
             animator.SetBool("isRunning", true);
             transform.position += movement2 * Time.deltaTime * moveSpeed;
         }
 
     }
-
-
 }
